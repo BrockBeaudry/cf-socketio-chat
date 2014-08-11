d81b7c97e1cf66ff36b187c455aa35d8d2eb27f6
David Davidson <daviddavidson.1988@gmail.com>
2014-08-11 11:02:28 -0700

    Add a lot of boilerplate, most of it copied verbatim from the Internet

--- static/helloWorld.js
+++ static/helloWorld.js
@@ -1,5 +1,7 @@
-var socket = io.connect('http://localhost');
-socket.on('news', function(data) {
-	console.log(data);
-	socket.emit('event 2', {hello: 'again'});
+$(document).on('ready', function() {
+	var socket = io.connect('http://localhost');
+	socket.on('news', function(data) {
+		console.log(data);
+		socket.emit('event 2', {hello: 'again'});
+	});
 });
